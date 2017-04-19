6b4a1bf5c6c99c48b10099bf1469f8f548fb7eb2
Tor Borgen <Tor Borgen>
2017-04-19 15:09:36 +0200

    more correction in form

--- mainWeb.go
+++ mainWeb.go
@@ -351,7 +351,7 @@ func formInputHandler(w http.ResponseWriter, r *http.Request) {
 		byte.WriteString("http://158.37.63.236:8080/speech?text=" + urlText)
 
 		if r.Form.Get("pitch") != "" {
-			byte.WriteString("&pitch=" + r.Form.Get("pitch"))
+			byte.WriteString("&pitch=" + r.Form.Get("pit	ch"))
 				//<0, 99; default 50>]
 		}
 		if r.Form.Get("speed") != "" {
